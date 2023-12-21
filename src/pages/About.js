4e4bc584d98f2c68991ccd3ed0c1beaa82471180
AbdelmajidAbdessadek <abdessadekabdelmajid85@gmail.com>
2023-12-21 02:14:42 +0100

    react-app

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navigation from '../componenets/Navigation';
+const About = () => {
+    return (
+      <div>
+        <Navigation />
+        <h1>A propos</h1>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+          obcaecati delectus et illo facere tempora aspernatur nisi voluptatibus
+          minima omnis sint perspiciatis officia molestiae officiis perferendis,
+          possimus quas repudiandae. Corrupti. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Quaerat molestias labore suscipit sunt
+          ad aperiam rem maiores? Facilis consequuntur optio recusandae eaque,
+          cumque excepturi. Incidunt possimus dolor deleniti doloremque eaque.
+        </p>
+      </div>
+    );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../componenets/Navigation';
+const Home = () => {
+    return (
+      <div>
+        <Navigation />
+        <h1>ACCEUIL</h1>
+      </div>
+    );
+};
+
+export default Home;
